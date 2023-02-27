d93a82fdcdf513bbb1d005977f80e5c8bd2ff238
Kanishk Pachauri <krishna03012003@icloud.com>
2023-02-27 23:57:15 +0530

    Created a server and api

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+
+my_webpage = requests.get('http://127.0.0.1:5000/')
+
+my_webpage_data = my_webpage.text
+
+my_soup = BeautifulSoup(my_webpage_data, 'html.parser')
+
+
+
+get_temperature = (my_soup.find(id="temperature").get_text())
+get_humidity = (my_soup.find(id='humidity').get_text())
+
+
+from flask import Flask, jsonify, request
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/temperature', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+
+        return jsonify({'temperature': get_temperature})
+  
+@app.route('/humidity', methods = ['GET', 'POST'])
+def humidity():
+    if(request.method == 'GET'):
+
+        return jsonify({'humidity': get_humidity})
+    
+
+
+if __name__ == '__main__':
+  
+    app.run(port=5050 ,debug = True)
