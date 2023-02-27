d389de41c0ec6caa35c81a8f874fa2905839996c
Kanishk Pachauri <krishna03012003@icloud.com>
2023-02-28 01:15:40 +0530

    Added recursive search

--- api/main.py
+++ api/main.py
@@ -3,17 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 
 # my_webpage = requests.get('http://127.0.0.1:5000/')
-my_webpage = requests.get('https://fluffy-bubblegum-36cf63.netlify.app/')
 
 
-my_webpage_data = my_webpage.text
-
-my_soup = BeautifulSoup(my_webpage_data, 'html.parser')
-
-
-
-get_temperature = (my_soup.find(id="temperature").get_text())
-get_humidity = (my_soup.find(id='humidity').get_text())
 
 
 from flask import Flask, jsonify, request
@@ -26,13 +17,20 @@ app = Flask(__name__)
 @app.route('/temperature', methods = ['GET', 'POST'])
 def home():
     if(request.method == 'GET'):
+        my_webpage = requests.get('https://fluffy-bubblegum-36cf63.netlify.app/')
+        my_webpage_data = my_webpage.text
+        my_soup = BeautifulSoup(my_webpage_data, 'html.parser')
+        get_temperature = (my_soup.find(id="temperature").get_text())
 
         return jsonify({'temperature': get_temperature})
   
 @app.route('/humidity', methods = ['GET', 'POST'])
 def humidity():
     if(request.method == 'GET'):
-
+        my_webpage = requests.get('https://fluffy-bubblegum-36cf63.netlify.app/')
+        my_webpage_data = my_webpage.text
+        my_soup = BeautifulSoup(my_webpage_data, 'html.parser')
+        get_humidity = (my_soup.find(id="temperature").get_text())
         return jsonify({'humidity': get_humidity})
     
 
